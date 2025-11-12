b2896ca131c66cee56d88cdbeac667bb62d9e587
Wruhanto Jati <jwruhanto@gmail.com>
2025-11-12 13:25:29 +0000

    terlalu transparan (_for element <Slot.SlotClone>_)

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -54,7 +54,7 @@ const buttonVariants = cva(
         primary: `${glassmorphismEffect} bg-black/60 text-white hover:shadow-black/20 dark:bg-white/20 dark:text-black before:bg-black/50 dark:before:bg-white/50`,
         destructive:
           "bg-destructive text-white shadow-xs hover:bg-destructive/90 dark:bg-destructive/60",
-        secondary: `${glassmorphismEffect} bg-white/20 border border-white/20 text-foreground hover:shadow-white/20 dark:bg-black/30 before:bg-white/60 dark:before:bg-black/60`,
+        secondary: `${glassmorphismEffect} bg-white/40 border border-white/40 text-foreground hover:shadow-white/20 dark:bg-black/30 before:bg-white/60 dark:before:bg-black/60`,
         ghost: "hover:bg-accent dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
         glass: `${glassmorphismEffect} bg-white/20 text-foreground hover:shadow-primary/20 dark:bg-black/20 before:bg-white/50 dark:before:bg-black/50`,
