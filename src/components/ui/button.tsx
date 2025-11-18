45d77f478ab66562bb47a9208159638da836da73
Wruhanto Jati <jwruhanto@gmail.com>
2025-11-18 04:28:05 +0000

    mulai bertahap, mulai dari hero dulu, tapi biarkan lokasi tombol play ny

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,3 +1,4 @@
+
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
@@ -58,7 +59,7 @@ const buttonVariants = cva(
         secondary: `bg-secondary hover:bg-secondary/80 text-secondary-foreground`,
         ghost: "hover:bg-accent dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
-        frosted: `${frostedGlassEffect} bg-white/45 border border-white/60 saturate-150 dark:bg-white/10 dark:border-white/20 before:bg-white/60 dark:before:bg-white/25`,
+        frosted: `${frostedGlassEffect} bg-white/45 border border-white/60 saturate-150 text-foreground dark:bg-black/20 dark:border-white/20 before:bg-white/60 dark:before:bg-black/25`,
       },
       size: {
         default: "h-10 sm:h-11 px-4 text-sm",
