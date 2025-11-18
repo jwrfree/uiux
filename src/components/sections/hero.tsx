a8437a22829d2c815828d575e1ca04bada51a4d0
Wruhanto Jati <jwruhanto@gmail.com>
2025-11-18 03:21:00 +0000

    sekarnag yg animasi scroll down, dia mengahlangi running text

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -193,7 +193,7 @@ const HeroSection = () => {
 
       {/* Bottom Controls */}
       <motion.div
-        className="absolute z-[2] bottom-10 left-1/2 -translate-x-1/2 w-full flex justify-center items-end"
+        className="absolute z-[2] bottom-24 left-1/2 -translate-x-1/2 w-full flex justify-center items-end"
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 1, duration: 0.8, ease: "easeOut" }}
@@ -253,5 +253,3 @@ const HeroSection = () => {
 };
 
 export default HeroSection;
-
-    
